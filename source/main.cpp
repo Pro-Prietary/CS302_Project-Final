6c40e05d8785a0cb93aa22d7c531b5225277f5ca
conner991 <32372555+conner991@users.noreply.github.com>
2020-04-27 23:33:00 -0700

    kernCommit

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,26 @@
+/* -----------------------------------------------------------------------------
+FILE NAME:         
+DESCRIPTION:       
+USAGE:             
+COMPILER:          GNU g++ compiler on Linux
+NOTES:             
+
+MODIFICATION HISTORY:
+Author             	Date               Version
+---------------    	----------         --------------
+Conner Fissell
+NJ Pelpinosas     	         			1.0  Original version
+----------------------------------------------------------------------------- */
 #include <iostream>
 #include <fstream>
 
-int main( void )
+/* -----------------------------------------------------------------------------
+FUNCTION:          
+DESCRIPTION:       
+RETURNS:           
+NOTES:             
+------------------------------------------------------------------------------- */
+int main(void)
 {
 	return 0;
 }
