2f75cad7f0600d310e32cd9d8771440ec4a37773
Narstrel Jordan Pelpinosas <njpelpinosas@gmail.com>
2020-04-27 23:35:00 -0700

    fix yer formatting conner

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,17 @@
 /* -----------------------------------------------------------------------------
-FILE NAME:         
-DESCRIPTION:       
-USAGE:             
-COMPILER:          GNU g++ compiler on Linux
-NOTES:             
+FILE NAME: main.cpp
+DESCRIPTION: main driver
+USAGE: ./output
+COMPILER: GNU g++ compiler on Linux
+NOTES: 
 
 MODIFICATION HISTORY:
 Author             	Date               Version
 ---------------    	----------         --------------
 Conner Fissell
-NJ Pelpinosas     	         			1.0  Original version
+NJ Pelpinosas     	4/27/2020          1.0  Original version
 ----------------------------------------------------------------------------- */
+
 #include <iostream>
 #include <fstream>
 
@@ -20,6 +21,7 @@ DESCRIPTION:
 RETURNS:           
 NOTES:             
 ------------------------------------------------------------------------------- */
+
 int main(void)
 {
 	return 0;
