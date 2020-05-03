6c5609850172d9ee3e707aa4ca673d16effcbdef
Conner <connerfissell91@gmail.com>
2020-05-03 00:02:02 -0700

    Got file stuff working?

--- source/main.cpp
+++ source/main.cpp
@@ -11,8 +11,20 @@ Author             	Date               Version
 Conner Fissell
 NJ Pelpinosas     	4/27/2020          1.0  Original version
 ----------------------------------------------------------------------------- */
-#include "Edge.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <boost/graph/adjacency_list.hpp>
 
+// Define datatypes, listS is a selector for the container used to represent the edge list for each
+// of the vertices. vecS is the selector for the conatainer used to represent the vertex list
+// of the graph.
+typedef boost::property<boost::edge_weight_t, int> EdgeWeightProperty;
+typedef boost::adjacency_list<boost::listS, boost::vecS, boost::bidirectionalS, boost::no_property, EdgeWeightProperty> Graph;
+
+
+void addEdge(std::string city1, std::string city2, int miles, Graph graphite);
 /* -----------------------------------------------------------------------------
 FUNCTION:          
 DESCRIPTION:       
@@ -22,11 +34,8 @@ NOTES:
 
 int main(void)
 {
-	std::vector<int> miles, numEdges;
-	std::vector<Edge> edges;
-	std::string tempCity;
-	int tempMiles;
-	int counter = 0;
+	std::string city1, city2, fcity, ncity;
+	int miles;
 
 	// Open up the file
 	std::fstream inFile;
@@ -36,52 +45,87 @@ int main(void)
 		std::cout << "\nError Opening File.\n\n";
 	}
 
-	int Reno = 0;
-	int San_Francisco = 1;
-	int Salt_Lake_City = 2;
-	int Seattle = 3;
-	int Las_Vegas = 4;
-	int SIZE_OF_ENUM = 5;
-
-	// Define datatypes, listS is a selector for the container used to represent the edge list for each
-	// of the vertices. vecS is the selector for the conatainer used to represent the vertex list
-	// of the graph.
-	typedef boost::property<boost::edge_weight_t, int> EdgeWeightProperty;
-	typedef boost::adjacency_list<boost::listS, boost::vecS, boost::bidirectionalS, boost::no_property, EdgeWeightProperty> Graph;
-
+	
 	// Instantiate a Graph object named graphite
 	Graph graphite(5);
 
-	Edge *e1 = new Edge;
-
-
 
 	// Read in cities and miles into vectors and create edges
-	while (inFile >> tempCity)
+	while (inFile >> city1)
 	{
-		city1.push_back(tempCity);
-		std::cin >> tempCity;
-		city2.push_back(tempCity);
-		std::cin >> tempMiles;
-		miles.push_back(tempMiles);
+		std::cin >> city2;
+		std::cin >> miles;
+		addEdge(city1, city2, miles, graphite);
 	}
 
-	for (int i = 0; i < counter; i++)
-	{
-	}
 
-	// Add weight edges to graphite
-	// integers located in each line are the distances between the cities
-	boost::add_edge(Reno, San_Francisco, 218, graphite);
-	boost::add_edge(Reno, Salt_Lake_City, 518, graphite);
-	boost::add_edge(Reno, Seattle, 704, graphite);
-	boost::add_edge(Reno, Las_Vegas, 439, graphite);
-	boost::add_edge(San_Francisco, Seattle, 808, graphite);
-	boost::add_edge(San_Francisco, Las_Vegas, 569, graphite);
-	boost::add_edge(Salt_Lake_City, Seattle, 830, graphite);
-	boost::add_edge(Salt_Lake_City, Las_Vegas, 421, graphite);
+
 
 	//boost::dijkstra_shortest_paths( graphite, kkkkkkkk) // eh, i'll commit and get to it laater
 
 	return 0;
+}
+
+/* -----------------------------------------------------------------------------
+FUNCTION:          
+DESCRIPTION:       
+RETURNS:           
+NOTES:             
+------------------------------------------------------------------------------- */
+void addEdge(std::string city1, std::string city2, int miles, Graph graphite)
+{
+	std::string Reno, San_Francisco, Salt_Lake_City, Seattle, Las_Vegas;
+	int reno = 0;
+	int sf = 1;
+	int slc = 2;
+	int sea = 3;
+	int lv = 4;
+
+	if ((city1 == Reno) && (city2 == San_Francisco))
+	{
+		// Add weight edges to graphite
+		// integers located in each line are the distances between the cities
+		boost::add_edge(reno, sf, miles, graphite);
+	}
+
+	else if ((city1 == Reno) && (city2 == Salt_Lake_City))
+	{
+		boost::add_edge(reno, slc, miles, graphite);
+	}
+
+	else if ((city1 == Reno) && (city2 == Seattle))
+	{
+		boost::add_edge(reno, sea, miles, graphite);
+	}
+
+	else if ((city1 == Reno) && (city2 == Las_Vegas))
+	{
+		boost::add_edge(reno, lv, miles, graphite);
+	}
+
+	else if ((city1 == San_Francisco) && (city2 == Seattle))
+	{
+		boost::add_edge(sf, sea, miles, graphite);
+	}
+
+	else if ((city1 == San_Francisco) && (city2 == Las_Vegas))
+	{
+		boost::add_edge(sf, lv, miles, graphite);
+	}
+
+	else if ((city1 == San_Francisco) && (city2 == Seattle))
+	{
+		boost::add_edge(slc, sea, miles, graphite);
+	}
+
+	else if ((city1 == Salt_Lake_City) && (city2 == Las_Vegas))
+	{
+		boost::add_edge(slc, lv, miles, graphite);
+	}
+
+	else
+	{
+		std::cout << std::endl;
+	}
+	
 }
